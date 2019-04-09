c4b70bcc8ad9f91f254e3b65a26ffbfaca1f0787
Andrea Pavano <andpav@hotmail.it>
2019-04-09 09:50:10 +0200

    added TestConfig

---
+++ test/TestConfig/TestConfig/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestConfig
+{
+    //TODO: aggiungere ip e porta del server e il canale wi-fi su cui l'esp deve sniffare
+    class Configuration
+    {
+        public Configuration(long timestamp)
+        {
+            this.Timestamp = timestamp;
+        }
+
+        [JsonProperty(PropertyName = "timestamp")]
+        public long Timestamp
+        {
+            set; get;
+        }
+    }
+}
+
+
