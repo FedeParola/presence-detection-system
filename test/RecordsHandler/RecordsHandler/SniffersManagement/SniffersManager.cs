c1afb16f3b7f9597bfa09cf20ac3d078dbe71a0d
Andrea Pavano <andpav@hotmail.it>
2019-05-02 19:27:06 +0200

    .

--- test/RecordsHandler/RecordsHandler/SniffersManagement/SniffersManager.cs
+++ test/RecordsHandler/RecordsHandler/SniffersManagement/SniffersManager.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
@@ -9,6 +10,11 @@ using System.Threading;
 
 namespace RecordsHandler.SniffersManagement {
     class SniffersManager {
+        //il comparer eventualmente è da spostare in una classe a parte insieme al processamento/merging dei record
+        private IComparer comparer = new Comparer();
+
+        private DBManager db;
+
         private JsonSerializer serializer = new JsonSerializer();
         
         /* 
@@ -17,7 +23,7 @@ namespace RecordsHandler.SniffersManagement {
          * Value: list of raw records that needs to be processed
          */
         private ConcurrentDictionary<String, List<Record>> rawRecords = new ConcurrentDictionary<String, List<Record>>();
-
+        
         /* For every sniffer (identified by ip) tells if there are new records to process */
         private ConcurrentDictionary<String, bool> newRecordsFlags = new ConcurrentDictionary<string, bool>();
         private AutoResetEvent newRecordsEvent = new AutoResetEvent(false);
@@ -29,9 +35,11 @@ namespace RecordsHandler.SniffersManagement {
 
         public SniffersManager() {
             Port = 13000;
+            db = new DBManager("127.0.0.1", "user", "pass", "ai_esercitazione2");
 
             /* Initialize flags for all sniffers */
             newRecordsFlags["127.0.0.1"] = false;
+            newRecordsFlags["127.0.0.2"] = false;
         }
 
         public void ListenSniffers() {
@@ -64,6 +72,7 @@ namespace RecordsHandler.SniffersManagement {
             } finally {
                 /* Stop listening for new clients */
                 server.Stop();
+                db.closeConn();
             }
         }
 
@@ -129,6 +138,75 @@ namespace RecordsHandler.SniffersManagement {
 
                 /* Process records here */
 
+                Boolean found;
+                Boolean diffTimestamp;
+                Boolean nextRecord;
+                long timeTolerance = 1 * 1000; //1 second
+                var rawRecordsArray = rawRecords.ToArray();
+                int espCount = rawRecordsArray.Length;
+                /*Opt: Sort the array of rawRecords in ascending order for the number of records associated to each esp*/
+                Array.Sort(rawRecordsArray, 0, espCount, comparer);
+                /*Opt: mark the first packet considered for each list of packets; when working on the next packet
+                 * (which will be more recent) we can start looking for it among the the ones captured by the others esp32 
+                 * starting from the marked packet and ignoring the previous ones (they will be older)*/
+                int[] startIndex = new int[espCount];
+                for(int i = 0; i < espCount; i++)
+                {
+                    startIndex[i] = 0;
+                }
+                Boolean startIndexUpdated;
+                /*Go through the records of the first esp32*/
+                foreach (var record in rawRecordsArray[0].Value)
+                {
+                    nextRecord = false;
+                    /*Go through each esp32*/
+                    for (int i = 1; i < espCount && nextRecord == false; i++)
+                    {
+                        var recordsList = rawRecordsArray[i].Value.ToArray();
+                        found = false;
+                        diffTimestamp = false;
+                        startIndexUpdated = false;
+                        /*Go through each packet captured by the esp32*/
+                        for (int j = startIndex[i]; j < recordsList.Length && found == false && diffTimestamp == false; j++)
+                        {
+                            if(recordsList[j].Timestamp > record.Timestamp + timeTolerance)
+                            {
+                                /*The esp32 we are considering did not capture the record: 
+                                 * we can consider the next record and ignore this one*/
+                                diffTimestamp = true;
+                            }
+                            else if(recordsList[j].Timestamp > record.Timestamp - timeTolerance)
+                            {
+                                if(startIndexUpdated == false)
+                                {
+                                    startIndex[i] = j;
+                                    startIndexUpdated = true;
+                                }
+                                if(recordsList[j].Hash.Equals(record.Hash))
+                                {
+                                    /*The esp32 we are considering did capture the record: 
+                                     * we have to see if the others did the same*/
+                                    found = true;
+                                }
+                            }
+                        }
+                        if(diffTimestamp == true || found == false)
+                        {
+                            nextRecord = true;
+                        }
+                    }
+
+                    if(nextRecord == false)
+                    {
+                        /*The record was captured by each and any esp32*/
+                        /*Compute position*/
+                        double x = 0;
+                        double y = 0;
+                        /*Insert the record into the db*/
+                        db.insertRecord(record.Hash, record.MacAddr, record.Ssid, record.Timestamp, x, y);
+                    }
+                }
+
                 Console.WriteLine("(PROCESSOR) Records processed");
 
                 /* Mark all records as processed */
@@ -138,6 +216,16 @@ namespace RecordsHandler.SniffersManagement {
                 recordsProcessedEvent.Set();
                 Console.WriteLine("(PROCESSOR) All flags reset");
             }
+        }
+    }
+
+    class Comparer : IComparer
+    {
+        public int Compare(Object x, Object y)
+        {
+            var a = (KeyValuePair<String, List<Record>>)x;
+            var b = (KeyValuePair<String, List<Record>>)y;
+            return a.Value.Count.CompareTo(b.Value.Count);
         }
     }
 }
